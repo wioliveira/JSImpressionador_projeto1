70a78f1e0070a51b21e82fc60f74d8e65316b5d9
wioliveira <wioliveira@gmail.com>
2024-03-26 22:17:25 -0300

    Atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import { turboLogger } from './turboLogger.js';
 
 console.log('Estou usando o Github');
 turboLogger('Estou aprendendo o fluxo do git', 'red');
+turboLogger("Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema", 'darkgreen')
